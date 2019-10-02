f17da83c8ad73549356017f6800e733ca1595f06
Taiyan Zu <taiyanzu@gmail.com>
2019-10-01 21:38:47 -0400

    Added a Demo page

---
+++ TelephoneKeyPad/TelephoneKeyPad.Web/Controllers/DemoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelephoneKeyPad.Web.Models;
+
+namespace TelephoneKeyPad.Web.Controllers
+{
+    public class DemoController : Controller
+    {
+        private CombinationsVM exampleCombinations = new CombinationsVM
+        {
+            OriginalNumber = "2223334567",
+            ItemCount = 1000,
+            PageCount = 100,
+            PageSize = 10,
+            PageIndex = 0,
+            PagedItems = Enumerable.Repeat(Guid.NewGuid().ToString(), 10).ToArray()
+        };
+
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View(new CombinationsVM());
+        }
+
+        [HttpPost]
+        public ActionResult Index(CombinationsVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                // fake the generation
+                return View(exampleCombinations);
+            }
+            
+        }
+    }
+}
---
+++ TelephoneKeyPad/TelephoneKeyPad.Web/Models/CombinationsVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TelephoneKeyPad.Web.Models
+{
+    public class CombinationsVM
+    {
+        [DisplayName("Phone Number")]
+        public string OriginalNumber { get; set; }
+        public int ItemCount { get; set; }
+        public string[] PagedItems{ get; set; }
+        public int PageIndex { get; set; }
+        public int PageCount { get; set; }
+        public int PageSize { get; set; }
+    }
+}
